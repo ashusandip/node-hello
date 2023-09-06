c6286ff45cca6661d365cebdd3273fb2cbf31df0
ashusandip <133151400+ashusandip@users.noreply.github.com>
2023-09-06 19:02:07 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -7,13 +7,13 @@ sonarqubeScanner({
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
 	    'sonar.login': 'admin',
-      'sonar.password': 'SonarQube',
+            'sonar.password': 'SonarQube',
 	    'sonar.token': 'sqp_c65d9b8decfa27b3482887c1a04ae544ca25b367',
-      'sonar.projectVersion':'1.0',
+            'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
-      'sonar.sourceEncoding':'UTF-8',
-      'sonar.sources': '.',
-	  //'sonar.tests': 'specs',
-      //'sonar.inclusions' : 'src/**'
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	    //'sonar.tests': 'specs',
+           //'sonar.inclusions' : 'src/**'
        },
 }, () => {});
